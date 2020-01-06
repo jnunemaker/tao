176a3bf429127fd098efa49518195132da88f9c8
John Nunemaker <nunemaker@gmail.com>
2020-01-06 09:56:31 -0500

    Split client tests to object and association

---
+++ test/adapters/postgres/association_test.rb
@@ -0,0 +1,121 @@
+require 'test_helper'
+
+class AssociationPostgresAdapterTest < Minitest::Test
+  def setup
+    ActiveRecord::Base.connection.execute("TRUNCATE TABLE tao_associations")
+  end
+
+  def test_can_create_get_change_type_and_delete_association
+    client = Tao::Client.new
+
+    association = client.association.create(id1, friend_type, id2).value!
+    assert_instance_of Tao::Association, association
+    assert_equal 1, associations_count
+    now = Time.now.utc
+
+    friend_associations = client.association.get(id1, friend_type, [id2]).value!
+    assert_instance_of Array, friend_associations
+    friend_association = friend_associations.first
+    assert_instance_of Tao::Association, friend_association
+    assert_equal id1, friend_association.id1
+    assert_equal friend_type, friend_association.type
+    assert_equal id2, friend_association.id2
+    assert_in_delta now, friend_association.time, 2
+
+    result = client.association.change_type(id1, friend_type, id2, enemy_type).value!
+    assert result
+    assert_equal 1, associations_count
+
+    enemy_associations = client.association.get(id1, enemy_type, [id2]).value!
+    assert_instance_of Array, enemy_associations
+    enemy_association = enemy_associations.first
+    assert_instance_of Tao::Association, enemy_association
+    assert_equal id1, enemy_association.id1
+    assert_equal enemy_type, enemy_association.type
+    assert_equal id2, enemy_association.id2
+    assert_in_delta now, enemy_association.time, 2
+
+    assert client.association.delete(id1, enemy_type, id2).value!
+  end
+
+  def test_can_create_association_with_time_and_data
+    client = Tao::Client.new
+    time = Time.now.utc - 60
+    data = {"foo" => "bar"}
+    association = client.association.create(id1, friend_type, id2, time, data).value!
+    assert_instance_of Tao::Association, association
+    assert_equal 1, associations_count
+    assert_equal time, association.time
+    assert_equal data, association.data
+  end
+
+  def test_assocation_querying
+    day = 60 * 60 * 24
+    now = Time.now.utc
+    client = Tao::Client.new
+    friend_ids = []
+
+    (1..100).each do |n|
+      friend_id = n + 100
+      friend_ids << friend_id
+      time = now - (n * day)
+      client.association.create(id1, friend_type, friend_id, time).value!
+    end
+
+    assert_equal 100, client.association.count(id1, friend_type).value!
+    assert_equal 0, client.association.count(friend_ids.first, friend_type).value!
+
+    batch_friend_ids = friend_ids[11..20]
+    associations = client.association.get(id1, friend_type, batch_friend_ids).value!
+    assert_equal 10, associations.size
+    associations.each do |association|
+      assert_equal id1, association.id1
+      assert_equal friend_type, association.type
+      assert_includes friend_ids, association.id2
+    end
+
+    associations = client.association.get(id1, friend_type, friend_ids, high: now - (5 * day)).value!
+    assert_equal 96, associations.size
+
+    associations = client.association.get(id1, friend_type, friend_ids, low: now - (5 * day)).value!
+    assert_equal 5, associations.size
+
+    associations = client.association.range(id1, friend_type, offset: 10).value!
+    assert_equal 90, associations.size
+
+    associations = client.association.range(id1, friend_type, offset: 10, limit: 10).value!
+    assert_equal 10, associations.size
+    assert_equal friend_ids[10..19], associations.map(&:id2)
+
+    high = now - (5 * day)
+    low = now - (14 * day)
+    associations = client.association.time_range(id1, friend_type, high: high, low: low).value!
+    assert_equal 10, associations.size
+    associations.each { |association|
+      assert association.time.to_i >= low.to_i, "#{association.time} was not >= #{low}"
+      assert association.time.to_i <= high.to_i, "#{association.time} was not <= #{high}"
+    }
+  end
+
+  private
+
+  def associations_count
+    GitHub::SQL.value("SELECT COUNT(*) FROM tao_associations")
+  end
+
+  def id1
+    1
+  end
+
+  def id2
+    2
+  end
+
+  def friend_type
+    "friend"
+  end
+
+  def enemy_type
+    "enemy"
+  end
+end
--- test/client_test.rb
+++ test/client_test.rb
@@ -6,128 +6,10 @@ class TaoClientTest < Minitest::Test
     ActiveRecord::Base.connection.execute("TRUNCATE TABLE tao_associations")
   end
 
-  def test_can_create_read_update_and_delete_objects
+  def test_initialize
     client = Tao::Client.new
-    object = client.object.create("user").value!
-    assert_instance_of Tao::Object, object
-    assert_equal "user", object.type
-    assert object.id > 0, "#{object.id} expected to be greater than 0, but was not"
-    assert_equal 1, objects_count
-
-    read_object = client.object.get(object.id).value!
-    assert_instance_of Tao::Object, object
-    assert_equal "user", read_object.type
-    assert_equal object.id, read_object.id
-
-    updated_object = client.object.update(object.id, foo: "bar").value!
-    assert_instance_of Tao::Object, object
-    assert_equal "user", updated_object.type
-    assert_equal object.id, updated_object.id
-    assert_equal "bar", updated_object.data["foo"]
-
-    client.object.delete(object.id)
-    assert_nil client.object.get(object.id).value!
-  end
-
-  def test_can_create_get_change_type_and_delete_association
-    client = Tao::Client.new
-
-    o1 = client.object.create("user").value!
-    o2 = client.object.create("user").value!
-    association = client.association.create(o1.id, "friend", o2.id).value!
-    assert_instance_of Tao::Association, association
-    assert_equal 1, associations_count
-    now = Time.now.utc
-
-    friend_associations = client.association.get(o1.id, "friend", [o2.id]).value!
-    assert_instance_of Array, friend_associations
-    friend_association = friend_associations.first
-    assert_instance_of Tao::Association, friend_association
-    assert_equal o1.id, friend_association.id1
-    assert_equal "friend", friend_association.type
-    assert_equal o2.id, friend_association.id2
-    assert_in_delta now, friend_association.time, 2
-
-    result = client.association.change_type(o1.id, "friend", o2.id, "enemy").value!
-    assert result
-    assert_equal 1, associations_count
-
-    enemy_associations = client.association.get(o1.id, "enemy", [o2.id]).value!
-    assert_instance_of Array, enemy_associations
-    enemy_association = enemy_associations.first
-    assert_instance_of Tao::Association, enemy_association
-    assert_equal o1.id, enemy_association.id1
-    assert_equal "enemy", enemy_association.type
-    assert_equal o2.id, enemy_association.id2
-    assert_in_delta now, enemy_association.time, 2
-
-    assert client.association.delete(o1.id, "enemy", o2.id).value!
-  end
-
-  def test_can_create_association_with_time_and_data
-    client = Tao::Client.new
-
-    o1 = client.object.create("user").value!
-    o2 = client.object.create("user").value!
-    time = Time.now.utc - 60
-    data = {"foo" => "bar"}
-    association = client.association.create(o1.id, "friend", o2.id, time, data).value!
-    assert_instance_of Tao::Association, association
-    assert_equal 1, associations_count
-    assert_equal time, association.time
-    assert_equal data, association.data
-  end
-
-  def test_assocation_querying
-    day = 60 * 60 * 24
-    now = Time.now.utc
-    client = Tao::Client.new
-
-    user = client.object.create("user").value!
-    friends = []
-
-    (1..100).each do |n|
-      friend = client.object.create("user").value!
-      friends << friend
-      time = now - (n * day)
-      client.association.create(user.id, "friend", friend.id, time).value!
-    end
-
-    assert_equal 100, client.association.count(user.id, "friend").value!
-    assert_equal 0, client.association.count(friends.first.id, "friend").value!
-
-    friend_ids = friends[11..20].map(&:id)
-    associations = client.association.get(user.id, "friend", friend_ids).value!
-    assert_equal 10, associations.size
-    associations.each do |association|
-      assert_equal user.id, association.id1
-      assert_equal "friend", association.type
-      assert_includes friend_ids, association.id2
-    end
-
-    friend_ids = friends.map(&:id)
-    associations = client.association.get(user.id, "friend", friend_ids, high: now - (5 * day)).value!
-    assert_equal 96, associations.size
-
-    friend_ids = friends.map(&:id)
-    associations = client.association.get(user.id, "friend", friend_ids, low: now - (5 * day)).value!
-    assert_equal 5, associations.size
-
-    associations = client.association.range(user.id, "friend", offset: 10).value!
-    assert_equal 90, associations.size
-
-    associations = client.association.range(user.id, "friend", offset: 10, limit: 10).value!
-    assert_equal 10, associations.size
-    assert_equal friends[10..19].map(&:id), associations.map(&:id2)
-
-    high = now - (5 * day)
-    low = now - (14 * day)
-    associations = client.association.time_range(user.id, "friend", high: high, low: low).value!
-    assert_equal 10, associations.size
-    associations.each { |association|
-      assert association.time.to_i >= low.to_i, "#{association.time} was not >= #{low}"
-      assert association.time.to_i <= high.to_i, "#{association.time} was not <= #{high}"
-    }
+    assert_instance_of Tao::Adapters::Postgres::Object, client.object
+    assert_instance_of Tao::Adapters::Postgres::Association, client.association
   end
 
   def test_paper_example_works
@@ -169,10 +51,6 @@ class TaoClientTest < Minitest::Test
     client.association.create(comment.id, "liked_by", david.id).value!
     client.association.create(david.id, "likes", comment.id).value!
 
-    # count 'em up
-    assert_equal 7, objects_count
-    assert_equal 16, associations_count
-
     # get alice's friends
     friends = client.association.range(alice.id, "friend").value!
     assert_equal 3, friends.size
@@ -180,13 +58,5 @@ class TaoClientTest < Minitest::Test
 
     # bob's friend
     assert_equal [alice.id], client.association.range(bob.id, "friend").value!.map(&:id2)
-  end
-
-  def objects_count
-    GitHub::SQL.value("SELECT COUNT(*) FROM tao_objects")
-  end
-
-  def associations_count
-    GitHub::SQL.value("SELECT COUNT(*) FROM tao_associations")
   end
 end
